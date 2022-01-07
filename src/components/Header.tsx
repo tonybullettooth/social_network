811f2656d38347d650f06b09842c415824260386
tonybullettooth <_B_B_@mail.ru>
2022-01-07 16:22:44 +0300

    firstcommit

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src="https://avatars.mds.yandex.net/i?id=db94e9f09b4e6dd8f329f0e7156807a2-5614297-images-thumbs&n=13&exp=1"
+                alt=""/>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div><img
+                src="https://avatars.mds.yandex.net/i?id=7270849bbd5bb77f86f313dc5d71ce6d-5583685-images-thumbs&n=13&exp=1"
+                alt=""/>
+            </div>
+            <div>
+                <img
+                    src="https://avatars.mds.yandex.net/i?id=8a3351160bcb33f4d200aa08bfa0bdc5-5634817-images-thumbs&n=13&exp=1"
+                    alt=""/>
+                ava+decription
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+            </div>
+            <div>
+                <div>post 1</div>
+                <div>post 2</div>
+                <div>post 3</div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
