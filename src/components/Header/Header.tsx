3c91eaf8279e2e268f343588226ff844fc8e52d5
tonybullettooth <_B_B_@mail.ru>
2022-01-08 19:08:41 +0300

    added push-pull instructions to html

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import s from './Header.module.css';
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img
                 src="https://avatars.mds.yandex.net/i?id=db94e9f09b4e6dd8f329f0e7156807a2-5614297-images-thumbs&n=13&exp=1"
                 alt=""/>
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,29 +1,20 @@
 import React from 'react';
-
+import s from './Profile.module.css';
+import MyPosts from "./MyPosts/MyPosts";
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <div><img
                 src="https://avatars.mds.yandex.net/i?id=7270849bbd5bb77f86f313dc5d71ce6d-5583685-images-thumbs&n=13&exp=1"
                 alt=""/>
             </div>
             <div>
-                <img
+                <img className={s.ava}
                     src="https://avatars.mds.yandex.net/i?id=8a3351160bcb33f4d200aa08bfa0bdc5-5634817-images-thumbs&n=13&exp=1"
                     alt=""/>
                 ava+decription
             </div>
-            <div>
-                my posts
-                <div>
-                    new post
-                </div>
-            </div>
-            <div>
-                <div>post 1</div>
-                <div>post 2</div>
-                <div>post 3</div>
-            </div>
+<MyPosts/>
         </div>
     );
 };
