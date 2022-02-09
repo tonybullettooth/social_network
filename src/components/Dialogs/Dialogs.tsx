6423aa3517d17e0110881cf594d869c84d9683ad
tonybullettooth <_B_B_@mail.ru>
2022-02-09 22:00:18 +0300

    added push-pull instructions to html

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -22,22 +22,36 @@ const Message = (props: MessagePropsType) => {
 }
 
 const Dialogs = () => {
+    let dialogsData = [
+        {id: '1', name: 'Sonya'},
+        {id: '2', name: 'Oleg'},
+        {id: '3', name: 'German'},
+        {id: '4', name: 'Any'},
+        {id: '5', name: 'Vera'},
+    ]
+    let messagesData = [
+        {id: '1', message: 'Hi'},
+        {id: '2', message: 'Meow'},
+        {id: '3', message: 'Vzhuh'},
+        {id: '4', message: 'Yo'},
+        {id: '5', message: 'Yo'},
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name="Sonya" id="1"/>
-                <DialogItem name="Oleg" id="2"/>
-                <DialogItem name="German" id="3"/>
-                <DialogItem name="Any" id="4"/>
-                <DialogItem name="Vera" id="5"/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
             </div>
             <div className={s.messages}>
-                <Message message="Hi"/>
-                <Message message="Meow"/>
-                <Message message="Vzhuh"/>
-                <Message message="Yo"/>
-                <Message message="Yo"/>
-                <Message message="Yo"/>
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
+                <Message message={messagesData[2].message}/>
+                <Message message={messagesData[3].message}/>
+                <Message message={messagesData[4].message}/>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,13 @@ import s from ".//MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+    let postData = [
+        {id: '1', message: 'Hi', likeCounts: 15},
+        {id: '2', message: 'how are you?', likeCounts: 20},
+        {id: '3', message: 'super hi', likeCounts: 22},
+        {id: '4', message: 'It is my first project.', likeCounts: 13},
+        {id: '5', message: 'bye', likeCounts: 11},
+    ]
     return (
         <div className={s.postsBlock}>
             <div>
@@ -15,11 +22,11 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message="hi" likesCount="15"/>
-                <Post message="how are you?" likesCount="20"/>
-                <Post message="super hi" likesCount="25"/>
-                <Post message="It is my first project." likesCount="35"/>
-                <Post message="bye" likesCount="5"/>
+                <Post message={postData[0].message} likesCount={postData[0].likeCounts}/>
+                <Post message={postData[1].message} likesCount={postData[1].likeCounts}/>
+                <Post message={postData[2].message} likesCount={postData[2].likeCounts}/>
+                <Post message={postData[3].message} likesCount={postData[3].likeCounts}/>
+                <Post message={postData[4].message} likesCount={postData[4].likeCounts}/>
             </div>
         </div>
     );
